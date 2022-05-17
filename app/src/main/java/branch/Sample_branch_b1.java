8a3683445476848f0afec18628df68aa717fce80
riditalove <fabihaafrose69@gmail.com>
2022-05-18 01:44:50 +0600

    yes you made a branch b1

---
+++ app/src/main/java/branch/Sample_branch_b1.java
@@ -0,0 +1,27 @@
+package branch;
+
+public class Sample_branch_b1 {
+    String name;
+    float cgpa;
+
+    public Sample_branch_b1(String name, float cgpa) {
+        this.name = name;
+        this.cgpa = cgpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getCgpa() {
+        return cgpa;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCgpa(float cgpa) {
+        this.cgpa = cgpa;
+    }
+}
